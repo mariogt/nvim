86ec1dd42709fc896ee7c0b67b6dd94dc209cda5
MarioGT <mario@mariogt.com>
2023-12-25 13:59:03 -0300

    telescope_updates

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,13 @@ vim.keymap.set('n', '<leader>xs', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sx', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- required if you want to show dot files on telescope
+local telescope = require('telescope')
+telescope.setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
