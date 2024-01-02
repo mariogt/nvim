175b249c025857c3bc2a7ad1ee44ea08cd2e59de
MarioGT <mario@mariogt.com>
2024-01-02 03:04:20 -0300

    mason_auto_lsp_installler

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -1,2 +1,17 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup {
+  ensure_installed = {
+    "bashls",
+    "rust_analyzer",
+    "clangd",
+    "cssls",
+    "gopls",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "marksman",
+    "tsserver",
+    "zls"
+  }
+}
