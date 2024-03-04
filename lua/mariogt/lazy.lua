620d905069fb126b6b3405941d3efe6c406924d1
MarioGT <mario@mariogt.com>
2024-03-03 22:33:24 -0300

    bye bye ruby - cobalt theme

--- lua/mariogt/lazy.lua
+++ lua/mariogt/lazy.lua
@@ -28,6 +28,14 @@ require("lazy").setup({
   { 'craftzdog/solarized-osaka.nvim' },
   { 'rebelot/kanagawa.nvim' },
   { "rose-pine/neovim",              name = "rose-pine" },
+  {
+    "lalitmee/cobalt2.nvim",
+    event = { "ColorSchemePre" }, -- if you want to lazy load
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+    init = function()
+      require("colorbuddy").colorscheme("cobalt2")
+    end,
+  },
   -- status line
   {
     'nvim-lualine/lualine.nvim',
--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -14,6 +14,6 @@ require("mason-lspconfig").setup {
     "tsserver",
     "zls",
     "pylsp",
-    "ruby_ls",
+    --"ruby_ls",
   }
 }
