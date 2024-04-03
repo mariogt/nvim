dd08627d07d34d6033fedd3fcac6d9d6b48a542b
MarioGT <mario@mariogt.com>
2024-04-03 14:43:53 -0300

    more config stuff

--- lua/mariogt/set.lua
+++ lua/mariogt/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver100-iCursor-blinkwait300-blinkon200-blinkoff150,r-cr-o:hor20"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
@@ -10,7 +10,6 @@ vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
@@ -21,6 +20,10 @@ vim.opt.colorcolumn = "80"
 vim.opt.showmode = true
 vim.opt.showcmd = true
 vim.opt.autoread = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
 
 vim.opt.background = 'dark'
 vim.cmd.colorscheme('gruvbox')
