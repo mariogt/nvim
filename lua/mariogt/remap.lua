7b79374900f5c63b4fb864d70d2141b9113e87c4
MarioGT <mario@mariogt.com>
2024-04-02 22:57:07 -0300

    harpoon2 fixes key remap

--- lua/mariogt/remap.lua
+++ lua/mariogt/remap.lua
@@ -77,3 +77,17 @@ vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
 vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
+
+local harpoon = require("harpoon")
+
+-- harpoon2
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<A-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<A-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<A-l>", function() harpoon:list():select(4) end)
+
--- lua/plugin/harpoon.lua
+++ lua/plugin/harpoon.lua
@@ -1,12 +1,23 @@
-local harpoon = require("harpoon")
+local harpoon = require('harpoon')
+harpoon:setup({})
 
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
 
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<leader>e", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<leader>d", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
