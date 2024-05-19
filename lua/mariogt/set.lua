0c958fbe1f609769088236865505f38263945de7
MarioGT <mario@mariogt.com>
2024-05-18 22:23:50 -0400

    color column on

--- lua/mariogt/set.lua
+++ lua/mariogt/set.lua
@@ -16,7 +16,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
---vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
 vim.opt.showmode = true
 vim.opt.showcmd = true
 vim.opt.autoread = true
