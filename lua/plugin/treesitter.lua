4e94442801de962cadaa4286afb5a87830c02baf
MarioGT <mario@mariogt.com>
2024-10-14 16:28:19 -0300

    new cmp mappings and treesitter regex

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "cpp", "lua", "vim", "vimdoc", "query", "html", "bash", "python" },
+  ensure_installed = { "javascript", "typescript", "c", "cpp", "lua", "vim", "vimdoc", "query", "html", "bash", "python", "regex" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
