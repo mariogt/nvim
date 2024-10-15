e533aba3f17c02ba0a3cc2405b20773e9998f5c4
MarioGT <mario@mariogt.com>
2024-10-15 12:25:20 -0300

    tabby setup

--- lua/plugin/tabbyml.lua
+++ lua/plugin/tabbyml.lua
@@ -1,2 +1,3 @@
---- Add config here. Example config:
-vim.g.tabby_keybinding_accept = '<Tab>'
+-- keybindings
+vim.g.tabby_keybinding_accept = '<C-x>'
+vim.g.tabby_keybinding_trigger_or_dismiss = '<C-_>'
