dd56fcd66161539d9335c2828764a52408a832cb
MarioGT <mario@mariogt.com>
2024-11-14 13:19:38 -0300

    tabby lazy setup

--- lua/mariogt/lazy.lua
+++ lua/mariogt/lazy.lua
@@ -105,8 +105,18 @@ require("lazy").setup({
   { 'SirVer/ultisnips' },
   { 'honza/vim-snippets' },
 
-  -- tabby ml
-  { 'TabbyML/vim-tabby' },
+  -- Tabby plugin
+  {
+    "TabbyML/vim-tabby",
+    lazy = false,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    init = function()
+      vim.g.tabby_agent_start_command = { "npx", "tabby-agent", "--stdio" }
+      vim.g.tabby_inline_completion_trigger = "auto"
+    end,
+  },
 
   -- vim-tmux-navigator
   { 'christoomey/vim-tmux-navigator' },
