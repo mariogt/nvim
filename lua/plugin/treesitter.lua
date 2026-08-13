186f423248c9a7c560b587789af8859b802125f3
MarioGT <mario@mariogt.com>
2026-08-13 09:46:07 -0400

    minor stuff

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,18 +1,18 @@
-require('nvim-treesitter').setup {}
-
-require('nvim-treesitter').install {
-  "javascript",
-  "typescript",
-  "c",
-  "cpp",
-  "lua",
-  "vim",
-  "vimdoc",
-  "query",
-  "html",
-  "bash",
-  "python",
-  "regex",
+require('nvim-treesitter').setup {
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "c",
+    "cpp",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "html",
+    "bash",
+    "python",
+    "regex",
+  },
 }
 
 vim.api.nvim_create_autocmd('FileType', {
